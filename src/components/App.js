a032bda68aaf4ae7bbc266c8d1cadb7f0b2648d4
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-03-04 13:13:40 +0100

    Style the main App in semantic ui grid

--- src/components/App.js
+++ src/components/App.js
@@ -26,11 +26,28 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        {
+          //Grid has 16 default columns}
+        }
+        <div className="ui grid">
+          <div className="ui row">
+            {
+              //Video detail will occupied the space of eleven columns out of 16
+            }
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            {
+              //Video list will occupied the space of five columns out of 16
+            }
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
