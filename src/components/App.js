e00c762be3ea5ef354ba4c700f28dfb0bac6d21d
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-03-04 13:21:59 +0100

    Add a default term search when components mounts

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,10 @@ import youtube from '../apis/youtube';
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit('What is componentDidMount');
+  }
+
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
